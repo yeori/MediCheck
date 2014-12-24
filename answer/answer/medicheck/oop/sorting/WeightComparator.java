c3092740841c4f7bbb7f31d1c5c66d8f149f6621
Yeori.Seo <yeori.seo@gmail.com>
2014-12-24 11:30:27 +0900

    패키지 이름 바로잡음 soring to sorting

---
+++ answer/answer/medicheck/oop/sorting/WeightComparator.java
@@ -0,0 +1,38 @@
+package answer.medicheck.oop.sorting;
+
+import java.util.Comparator;
+
+import quiz.medicheck.MedicalCheck;
+
+public class WeightComparator implements Comparator<MedicalCheck> {
+
+	
+	private boolean asc = true;
+	
+	public WeightComparator() {
+		this ( true );
+	}
+	
+	public WeightComparator(boolean asc) {
+		this.asc = asc;
+	}
+	
+	@Override
+	 
+	public int compare(MedicalCheck o1, MedicalCheck o2) {
+		double weight1 = o1.getWeight();
+		double weight2 = o2.getWeight();
+		int dir = asc ? 1 : -1;
+		
+		if (weight1 == weight2) {
+			return 0; 
+		}else if (weight1 > weight2){
+			return 1 * dir;
+		}else {
+			return -1 * dir;
+		}
+		
+		
+	}
+
+}
