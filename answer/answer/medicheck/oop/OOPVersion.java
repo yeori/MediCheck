eb3942967730cc743b523c541a9ad0afcb87cc96
Yeori.Seo <yeori.seo@gmail.com>
2014-12-24 13:54:44 +0900

    filter, mapper, sorter에 대한 의존을 최소화함 MediCheckContext 클래스에서 reflection으로 실제 구현체들을 로드함

--- answer/answer/medicheck/oop/OOPVersion.java
+++ answer/answer/medicheck/oop/OOPVersion.java
@@ -2,34 +2,22 @@ package answer.medicheck.oop;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
-import answer.medicheck.oop.filter.BoyFilter;
+import answer.medicheck.oop.context.MediCheckContext;
 import answer.medicheck.oop.filter.Filter;
-import answer.medicheck.oop.filter.GirlFilter;
-import answer.medicheck.oop.map.BmiMapper;
-import answer.medicheck.oop.map.HeightMapper;
 import answer.medicheck.oop.map.Mapper;
-import answer.medicheck.oop.map.WeightMapper;
-import answer.medicheck.oop.soring.HeightComparator;
 import quiz.medicheck.MedicalCheck;
 
 /**
- * 
- * 이름 : 서용열
- * 
- * 개발 : 8년 - 2년
- * 
- * 애플리케이션 - 미들웨어, ndrive( ndoc, excell, powerpoint )
- * 
- * 지금 - 한글파일을 웹브라우저에서 편지할 수 있는 프로그램(혼자)
- *      - 짬짬이 과외를 합니다.
- *      
- * 전공 - Test Egineer (시스템 개발을 하는데 테스트 가능성을 극대화해야함)
- *      - QA 는 아닙니다(제품의 결과가 스펙과 맞는지 보는 사람들)
  * 
  * 1. filtering - 일련의 집합(collection)에서 원하는 element들만 뽑아내는 역할.
  * 
@@ -43,46 +31,40 @@ import quiz.medicheck.MedicalCheck;
  * 1. printStudentByWeight와 printStudentByHeight 를 추상화해서 하나의 메소드를 사용하게 해봅시다.
  * 
  * 2. 다중정렬
- *    
- * @author Suhkyung
  *
  */
 public class OOPVersion {
 
-	public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-		
-		String sboyFilter = "answer.medicheck.oop.filter.BoyFilter"; // full qualified name
-		Class cls = Class.forName(sboyFilter);
-		
-		Filter<MedicalCheck> boyFilter = (Filter<MedicalCheck>) cls.newInstance();
+	public static void main(String[] args) throws FileNotFoundException {
 		
 		List<MedicalCheck> medicalCheckList = loadMedicalData();
 		
+		MediCheckContext ctx = new MediCheckContext("answer/medicheck/oop/config.txt");
 		
-		double avrWeight = average(medicalCheckList, new WeightMapper());
-		double avrHeight = average( medicalCheckList, new HeightMapper());
+		Mapper<Double, MedicalCheck> heightMapper = ctx.<Double>getMapper("height");
+		Mapper<Double, MedicalCheck> weightMapper = ctx.<Double>getMapper("weight");
+		Mapper<Double, MedicalCheck> bmiMapper = ctx.<Double>getMapper("bmi");
+		
+		double avrWeight = average(medicalCheckList, weightMapper);
+		double avrHeight = average( medicalCheckList, heightMapper);
 		
 		System.out.println("전체 학생의 평균 키     : " + avrHeight);
 		System.out.println("전체 학생의 평균 몸무게 : " + avrWeight);
 		
-		// 전체 학생의 평균키와 평균몸무게
-//		printAverageHeight(medicalCheckList, "전체학생 평균 키");
-//		printAverageWeight(medicalCheckList, "전체학생 평균 몸무게");
-		
 		// 성별 평균키 출력
+		Filter<MedicalCheck> girlFilter = ctx.getFilter("girl");
+		Filter<MedicalCheck> boyFilter = ctx.getFilter("boy");
+		double avrGirlHeight = average ( filtering ( medicalCheckList, girlFilter), heightMapper);
+		double avrBoyHeight = average ( filtering ( medicalCheckList, boyFilter), weightMapper);
 		
-		double avrGirlHeight = average ( filtering ( medicalCheckList, new GirlFilter()), new HeightMapper());
-		double avrBoyHeight = average ( filtering ( medicalCheckList, new BoyFilter()), new HeightMapper());
-		System.out.println("여학생의 평균 키     : " + avrGirlHeight);
+		System.out.println("여학생의 평균 키 : " + avrGirlHeight);
 		System.out.println("남학생의 평균 키 : " + avrBoyHeight);
 		
 		
-		double avrGrilBmi = average ( filtering(medicalCheckList, new GirlFilter()), new BmiMapper());
+		double avrGrilBmi = average ( filtering(medicalCheckList, girlFilter), bmiMapper);
 		System.out.println("여학생들 bmi 평균 : " + avrGrilBmi);
 		System.out.println();
-		
-		printBoysHeight(medicalCheckList, "남학생들 평균 키");
-		printGirlsHeight(medicalCheckList, "여학생들 평균 키");
+
 		
 		// 키가 가장 작은 학생, 가장 무거운 학생
 		printLowestStudent(medicalCheckList, "가장 키가 작은 학생");		
@@ -90,11 +72,23 @@ public class OOPVersion {
 
 		
 		// 정렬 - 키의 오름차순으로 출력하기 .... < ... < ....
-		printStudentByHeight(medicalCheckList, "키순서(오름차순)");
-
+		printStudents("키순서(오름차순)",
+				medicalCheckList, 
+				ctx.getComparator("height"), 
+				new OutputStreamWriter(System.out) );
+		
 		// 정렬 - 몸무게의 오름차순으로 출력하기
-		printStudentByWeight(medicalCheckList, "몸무게순서(오름차순)");
+		printStudents("몸무게순서(오름차순)",
+				medicalCheckList, 
+				ctx.getComparator("weight"), 
+				new OutputStreamWriter(System.out) );
 
+		// 정렬 - 여학생들만 몸무게의 내림차순으로 출력하기
+		
+		printStudents("여학생 몸무게순서(내림차순)",
+				filtering(medicalCheckList, girlFilter),
+				ctx.getComparator("weight.desc"),
+				new OutputStreamWriter(System.out) );
 		/*도전 과제 : 다중정렬
 		 * 
 		 * input.txt에는 키가 같은 학생이 세 명 있습니다.
@@ -141,8 +135,9 @@ public class OOPVersion {
 		
 		return total / mChecks.size();
 	}
+	
 	/**
-	 * b/medicheck/input.txt 를 읽어서 학생들의 신체검사 데이터를 List로 반환하는 메소드
+	 * input.txt 를 읽어서 학생들의 신체검사 데이터를 List로 반환하는 메소드
 	 * @return 신체검사 정보가 담긴 List
 	 * @throws FileNotFoundException
 	 */
@@ -165,88 +160,6 @@ public class OOPVersion {
 		scanner.close();
 		return medicalCheckList;
 	}
-	
-	
-	
-	/**
-	 * 전체 학생의 평균 키를 구해 화면으로 출력하는 메소드
-	 * @param medicalCheckList 각 학생의 신체검사 정보를 모은 리스트
-	 * @param title 화면 출력시 보여질 타이틀
-	 */
-	private static void printAverageHeight(
-			List<MedicalCheck> medicalCheckList,
-			String title ) {
-		
-		double totalH = 0;
-		for (int i = 0; i < medicalCheckList.size(); i++) {
-			totalH += medicalCheckList.get(i).getHeight();
-		}
-		
-		totalH = totalH / medicalCheckList.size();
-		// 화면으로 출력
-		System.out.println(String.format("[%s]", title));
-		System.out.println(String.format(" %.2f cm",totalH) );
-		System.out.println();
-	}
-	
-	/**
-	 * 전체 학생의 평균 몸무게를 구해 화면으로 출력하는 메소드
-	 * @param medicalCheckList 각 학생의 신체검사 정보를 모은 리스트
-	 * @param title 화면 출력시 보여질 타이틀
-	 */
-	private static void printAverageWeight(
-			List<MedicalCheck> medicalCheckList,
-			String title) {
-		
-		double totalW = 0;
-		for (int i = 0; i < medicalCheckList.size(); i++) {
-			totalW += medicalCheckList.get(i).getWeight();
-		}
-		totalW = totalW / medicalCheckList.size();
-		// 화면으로 출력
-		System.out.println(String.format("[%s]", title));
-		System.out.println(String.format(" %f kg",totalW) );
-		System.out.println();
-	}
-	
-	/**
-	 * 주어진 신체검사 정보 중 여학생들의 평균키를 화면으로 출력
-	 * @param medicalCheckList 모든 학생들의 신체검사 정보
-	 * @param title 화면 출력시 보여질 타이틀
-	 */
-	private static void printGirlsHeight(
-			List<MedicalCheck> medicalCheckList, 
-			String title) {
-		
-		List<MedicalCheck> girls = new ArrayList<MedicalCheck>();
-		for (int i = 0; i < medicalCheckList.size(); i++) {
-			if (medicalCheckList.get(i).getSex().equals("F")) {
-				girls.add(medicalCheckList.get(i));
-			}
-		}
-		// 화면으로 출력
-		printAverageHeight(girls, title);
-	}
-	
-	/**
-	 * 주어진 신체검사 정보 중 남학생들의 평균키를 화면으로 출력
-	 * @param medicalCheckList 모든 학생들의 신체검사 정보
-	 * @param title 화면 출력시 보여질 타이틀
-	 */
-	private static void printBoysHeight(
-			List<MedicalCheck> medicalCheckList,
-			String title) {
-		
-		List<MedicalCheck> boys = new ArrayList<MedicalCheck>();
-		for (int i = 0; i < medicalCheckList.size(); i++) {
-			if (medicalCheckList.get(i).getSex().equals("M")) {
-				boys.add(medicalCheckList.get(i));
-			}
-		}
-		// 화면으로 출력
-		printAverageHeight(boys, title);
-	}
-	
 	/**
 	 * 가장 키가 작은 학생을 화면에 출력
 	 * @param medicalCheckList 각 학생의 신체검사 정보를 모은 리스트
@@ -299,55 +212,75 @@ public class OOPVersion {
 		System.out.println();
 	}
 	
-
+	/**
+	 * 주어진 out을 PrintWriter로 캐스팅 또는 wrapping하는 메소드입니다.
+	 * 
+	 * PrintWriter는 print(..)와 println(...)의 메소드들이 있어서 문자들을 출력할때 자주 사용됩니다.
+	 * 
+	 * 현재 건네받은 out 변수가 참조하는 인스턴스의 타입이 PrintWriter이면 명시적으로 캐스팅을 하고
+	 * 그렇지 않으면 PrintWriter로 감싸서 반환해줍니다. 
+	 * @param out
+	 * @return
+	 */
+	final private static PrintWriter asPrintWriter ( Writer out) {
+		PrintWriter writer = null;
+		if ( out instanceof PrintWriter) {
+			writer = (PrintWriter) out;
+		} else {
+			writer = new PrintWriter ( out ) ;
+		}
+		return writer;
+	}
 	
 	/**
-	 * 몸무게의 내림차순으로 출력을 해주는 메소드
-	 * @param medicalCheckList
-	 * @param title
+	 * 주어진 신체검사 정보들을 정렬해서 출력하는 메소드입니다.
+	 * 
+	 * @param title - 타이틀
+	 * @param mChecks - 신체검사 정보들
+	 * @param comp - 신체검사 정보 정렬에 사용되는 comparator
+	 * @param out - 출력 스트림
 	 */
-	private static void printStudentByWeight(
-			List<MedicalCheck> medicalCheckList,
-			String title) {
+	private static void printStudents (String title,
+			List<MedicalCheck> mChecks , 
+			Comparator<MedicalCheck> comp, 
+			Writer out) {
+		/*
+		 * printStudents 를 호출하는 쪽(여기서는 main메소드)에서는 원하는 구체적인 
+		 * 출력 방향(파일인지, 콘솔인지, 네트워크인지)을 지정해서 호출하고,
+		 * 이 메소드안에서는 출력의 실제 구현을 추상 클래스인 Writer 타입으로 참조하기 때문에
+		 * 구체적인 출력방향에 대해서는 알 필요가 없게 합니다.
+		 * 
+		 * out 파라미터를 통해서 스트림 참조를 건네받은 printStudents메소드 안에서는
+		 * Writer.println 등의 메소드로 뭔가를 스트림에 써넣을 수 있다는 것만 알면 되고
+		 * 써 넣은 데이터들이 어디로 흘러가는지는 신경쓸 필요가 없습니다.
+		 * 
+		 * 이를 통해서 호출하는 쪽(main 메소드)과 호출되는 쪽(printStudents)의 책임을 분리해서
+		 * 유연한 코드를 작성할 수 있습니다.
+		 * 
+		 * 
+		 */
+		PrintWriter writer = asPrintWriter(out);
 		
-		HeightComparator comp = new HeightComparator();
-		Collections.sort(medicalCheckList, comp); // students.sort(comp);
-
-		// 화면으로 출력
-		System.out.printf("[%s]\n", title);
-		for( int i = 0 ; i < medicalCheckList.size(); i++) {
-			MedicalCheck aStudentData = medicalCheckList.get(i);
-			System.out.println(String.format("%2d %4s %4s %3.1fcm",
-					(i+1),
-					aStudentData.getStudentID(),
-					aStudentData.getName(),
-					aStudentData.getHeight()));
-		}
-		System.out.println();
-	}
-	/**
-	 * 키의 오름차순으로 출력을 해주는 메소드
-	 * @param medicalCheckList
-	 * @param title
-	 */
-	private static void printStudentByHeight(
-			List<MedicalCheck> medicalCheckList,
-			String title) {
+		Collections.sort(mChecks, comp);
 		
-		HeightComparator comp = new HeightComparator();
-		Collections.sort(medicalCheckList, comp); // students.sort(comp);
-
-		// 화면으로 출력
-		System.out.printf("[%s]\n", title);
-		for( int i = 0 ; i < medicalCheckList.size(); i++) {
-			MedicalCheck aStudentData = medicalCheckList.get(i);
-			System.out.println(String.format("%2d %4s %4s %3.1fcm",
-					(i+1),
-					aStudentData.getStudentID(),
-					aStudentData.getName(),
-					aStudentData.getHeight()));
+		Iterator<MedicalCheck> itr = mChecks.iterator();
+		int cnt = 1;
+		writer.printf("[%s]\n", title);
+		while ( itr.hasNext() ) {
+			MedicalCheck mc = itr.next();
+			writer.println(String.format("%2d %4s %4s %3.1fcm",
+					cnt++,
+					mc.getStudentID(),
+					mc.getName(),
+					mc.getHeight()));
 		}
-		System.out.println();
+		writer.println();
+		writer.flush();
+		// 여기서는 스트림을 닫지 않습니다.
+		// writer 변수가 참조하는 스트림이 printStudents 메소드 안에서 생성되지 않았기 때문에
+		// 함부로 아래와같이 스트림을 닫는 것은 오작동을 초래할 수 있습니다.
+		// 스트림은 반드시 생성한 쪽에서만 close() 하도록 코드를 작성하는 것이 좋습니다.
+		//writer.close();
 	}
 
 }
